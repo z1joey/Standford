294d7ce646f1e79a94c1fbe86ec20049604ad31d
Yi Zhang <yizhang@macdemac-mini.local>
2018-12-30 01:58:44 +0800

    Initial Commit

---
+++ Standford/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Standford
+//
+//  Created by Yi Zhang on 2018/12/30.
+//  Copyright © 2018 Yi Zhang. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
